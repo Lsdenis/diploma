42db8c946b5a483a3f02f74ec4b39e9aa791b15f
Denis P <lsden1995@gmail.com>
2014-03-20 21:36:34 +0300

    First commit, create a project

---
+++ WHS.Presentation/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WHS.Presentation.Resources;
+
+namespace WHS.Presentation
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
