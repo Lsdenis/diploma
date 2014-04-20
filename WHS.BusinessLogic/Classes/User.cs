97386bd6c8f7b824fe5e106b0374babc543bad3e
Denis P <lsden1995@gmail.com>
2014-04-20 14:25:25 +0300

    Change destination OS to 8.1, add new feature

---
+++ WHS.BusinessLogic/Classes/User.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace WHS.BusinessLogic.Classes
+{
+    class User
+    {
+        [SQLite.PrimaryKey, SQLite.AutoIncrement]
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public int Password { get; set; }
+    }
+}
---
+++ WHS.Presentation/Pages/LoginPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace WHS.Presentation.Pages
+{
+    public partial class LoginPage : PhoneApplicationPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
